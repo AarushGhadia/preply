2af8ca1f62125c5a7bc2517b52160275642c038a
Aarush Ghadia <aarush.ghadia@gmail.com>
2026-01-19 13:28:22 -0500

    feat:firstreactproject

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,28 @@
-
-
 const App = () => {
   return (
     <div>
-      <div className="flex justify-center items-center gap-10">
-        <div>logo</div>
+      <div className="flex justify-center items-center gap-100">
+        <div style={{ marginTop: '50px' }}>Logo</div>
         <div>
-          <div className="flex justify-between list-none gap-2">
+          <div className="flex list-none gap-10" style={{ marginLeft: '-300px', marginTop: '50px' }}>
             <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Protfolio</li>
-            <li>Pricing</li>
+            <li style={{opacity: 0.3}}>About</li>
+            <li style={{opacity: 0.3}}>Services</li>
+            <li style={{opacity: 0.3}}>Protfolio</li>
+            <li style={{opacity: 0.3}}>Pricing</li>
           </div>
         </div>
-        <div>button</div>
+        <div style={{ marginTop: '50px', marginLeft: '200px', borderWidth: '2px', borderStyle: 'solid', borderColor: 'purple', borderRadius: '10px', padding: '10px', backgroundColor: 'purple', width: '150px', textAlign: 'center' }}>Contact Us</div>
       </div>
-      <div className="flex justify-center items-center gap-10">
+      <div className="flex gap-10" style={{ marginTop: '100px', marginLeft: '100px' }}>
         <div>
           <div>
-            <h1>Heading</h1>
-            <div>Paragraph</div>
+            <h1 style={{ fontSize: '50px', fontWeight: 'bold', lineHeight: '1.2' }}>We Help You <br /><span style={{ color: 'purple' }}>Crush</span> Your <br />Competition</h1>
+            <div style={{ marginTop: '25px', color: 'gray' }}>Build trust to connect your brand to billions of <br />users and grow your revenue through SEO.</div>
           </div>
-          <div>button</div>
+          <div style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: 'purple', borderRadius: '10px', padding: '15px', backgroundColor: 'purple', marginTop: '50px', width: '200px', textAlign: 'center' }}>See Our Services</div>
         </div>
-        <div>Image</div>
+        <div style={{ marginLeft: '500px', marginTop: '100px'}}>Image</div>
       </div>
     </div>
   );
--- sandbox/js/utils.js
+++ sandbox/js/utils.js
@@ -1,26 +1,9 @@
-// const user = {
-//     name: "Alice",
-//     age: 25,
-//     city: "New York"
-// }
-
-// user.phone = "123-456-7890";
-
-// console.log(user);
-
-// function fuction_defination(){
-//     return "This is a function definition.";
-// }
-
-// const fuction_defination = () => {
-//     return "This is a function definition.";
-// }
-
-
-function add(a, b) {
-
-    return a + b;
+const user = {
+    name: "Alice",
+    age: 30,
+    isLoggedIn: true
 }
 
-console.log(add());
-
+const checkUserLoggedIn = (user) => {
+    return user.isLoggedIn;
+}

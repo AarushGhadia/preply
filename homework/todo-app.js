d3aeea08c53ed23e80de118d1a8e5e88348b941f
manishdashsharma <mdashsharma95@gmail.com>
2026-01-20 00:52:02 +0530

    feat: add quiz functionality with question loading, answer selection, and scoring

---
+++ homework/todo-app.js
@@ -0,0 +1,26 @@
+let todos = [];
+let nextId = 1;
+
+function addTodo(title) {
+  // add a new todo with id, title, completed=false
+}
+
+function getTodos() {
+  // return all todos
+}
+
+function completeTodo(id) {
+  // find todo by id and mark completed=true
+}
+
+function deleteTodo(id) {
+  // remove todo by id
+}
+
+function resetTodos() {
+  // clear all todos and reset id
+}
+
+
+
+

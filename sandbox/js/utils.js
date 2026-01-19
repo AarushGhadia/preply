fe73e913b448a63705f67a8ae1fb60647981fb9f
Aarush Ghadia <aarush.ghadia@gmail.com>
2026-01-19 14:15:57 -0500

    feat: implement quiz functionality with question loading and answer selection

--- sandbox/js/utils.js
+++ sandbox/js/utils.js
@@ -1,9 +1,60 @@
-const user = {
-    name: "Alice",
-    age: 30,
-    isLoggedIn: true
+const questions = [
+  {
+    question: "Capital of India?",
+    options: ["Mumbai", "Delhi", "Chennai"],
+    answer: "Delhi"
+  },
+  {
+    question: "2 + 2 = ?",
+    options: ["3", "4", "5"],
+    answer: "4"
+  }
+];
+
+let currentQuestionIndex = 0;
+let selectedAnswers = null
+let score = 0;
+
+function loadQuestion() {
+    if(currentQuestionIndex < questions.length) {
+        return {
+            question: questions[currentQuestionIndex].question,
+            options: questions[currentQuestionIndex].options
+        }
+    }
+    return null;
 }
 
-const checkUserLoggedIn = (user) => {
-    return user.isLoggedIn;
+function selectAnswer(selectedOption) {
+    selectedAnswers = selectedOption;
+    return selectedAnswers;
 }
+
+function nextQuestion() {
+  if(selectedAnswers === questions[currentQuestionIndex].answer) {
+    score++;
+  }
+  currentQuestionIndex++
+}
+
+function showResult() {
+  return {
+    score: score,
+    total: questions.length
+  }
+}
+
+function resetQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  selectedAnswers = null;
+}
+
+console.log(loadQuestion());
+console.log(selectAnswer("Delhi"));
+console.log(selectedAnswers);
+console.log(nextQuestion());
+console.log(loadQuestion());
+console.log(selectAnswer("4"))
+console.log(nextQuestion())
+console.log(showResult());

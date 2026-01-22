3cec830cd36933ddb191e169f7c8da8c0485c2d4
Aarush Ghadia <aarush.ghadia@gmail.com>
2026-01-22 13:18:07 -0500

    feat: enhance todo functionality with default parameters and improved logic

--- homework/todo-app.js
+++ homework/todo-app.js
@@ -3,24 +3,43 @@ let nextId = 1;
 
 function addTodo(title) {
   // add a new todo with id, title, completed=false
+  todos.push({ id: nextId, title, completed: false });
+  nextId++;
 }
 
 function getTodos() {
   // return all todos
+  return todos;
 }
 
-function completeTodo(id) {
+function completeTodo(id = 1) {
   // find todo by id and mark completed=true
+  console.log(id)
+  if (todos.includes(id)) {
+    todos[0].completed = true;
+  } else {    
+    return "Todo not found";
+  }
 }
 
-function deleteTodo(id) {
+function deleteTodo(id = 1) {
   // remove todo by id
+  if (todos.includes(id)) {
+    todos.splice(0, 1);
+  } else {    
+    return "Todo not found";
+  }
 }
 
 function resetTodos() {
   // clear all todos and reset id
+  todos = [];
+  nextId = 1;
 }
 
-
-
-
+console.log(addTodo("Task"));
+console.log(getTodos());
+console.log(addTodo("Task 2"));
+console.log(getTodos());
+console.log(completeTodo(2));
+console.log(getTodos());

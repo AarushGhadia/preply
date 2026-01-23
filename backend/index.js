d652c251bb27f6f49dc31e9d63ebd23ad9f50ae7
manishdashsharma <mdashsharma95@gmail.com>
2026-01-24 00:38:11 +0530

    feat: update root endpoint response and add addition functionality

--- backend/index.js
+++ backend/index.js
@@ -12,25 +12,34 @@ const PORT = 3000;
 // 1. The variable
 // 2. A callback function
 
+
 app.get('/',(req,res)=>{
     return res
     .status(200)
     .json({
         success:true,
-        message:"This is my first API",
+        message:"Backend is working! YAY!",
     })
 })
 
-app.get('/health',(req,res)=>{
+app.get('/add/:num1/:num2',(req,res)=>{
+
+    console.log(req.params);
+    console.log(req.params.num1);
+    console.log(req.params.num2);
+    
+    const num1 = Number(req.params.num1);
+    const num2 = Number(req.params.num2);
+    const sum = num1 + num2;
     return res
     .status(200)
     .json({
         success:true,
-        message:"The backend server is healthy",
+        message:`The sum of ${num1} and ${num2} is ${sum}`,
     })
 })
 
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
